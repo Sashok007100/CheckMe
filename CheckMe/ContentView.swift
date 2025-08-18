2c581aad08de539540eb1374a937e9bf50bf5f19
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-08-18 20:15:00 +0300

    Настроенный слайдер из UIKit был добавлен в представление.

--- CheckMe/ContentView.swift
+++ CheckMe/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue = 50.0
+    @State private var showAlert = false
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("Подвиньте слайдер, как можно ближе к: \(targetValue)")
+            
+            HStack{
+                Text("0")
+                UISliderRepresentable(currentValue: $currentValue, targetValue: $targetValue)
+                Text("100")
+            }
+            
+            Button("Проверь меня!") {
+                score = computeScore()
+                showAlert = true
+            }
+            
+            Button("Начать заново") {
+                randomTargetValue()
+            }
         }
         .padding()
+        .alert(
+            "Your Score",
+            isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text("\(score)")
+            }
+
+    }
+    
+    private func randomTargetValue() {
+        targetValue = Int.random(in: 0...100)
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
