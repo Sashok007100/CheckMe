47391310fd08a28c3de4ebcc35631bf68eeaa582
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-08-18 11:18:17 +0300

    Initial Commit

---
+++ CheckMe/CheckMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMeApp.swift
+//  CheckMe
+//
+//  Created by Alexandr Artemov (Mac Mini) on 18.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CheckMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
